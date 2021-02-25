67004b1c584c34f2cfe2222d744963d425dca883
Johnny Tae <tjohnny93@berkeley.edu>
2021-02-25 17:03:41 +0900

    Add: installed and added custom splash screen on start

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
 import { ScrollView, StyleSheet, Text, View, StatusBar } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
+import * as SplashScreen from 'expo-splash-screen';
 
 import Tabs from './src/navigatior/Tabs';
 
 const Stack = createStackNavigator();
 
+SplashScreen.preventAutoHideAsync()
+  .then(result => console.log(result, 'Splash Screen Loading'))
+  .catch(console.warn);
+
 export default function App() {
+  useEffect(() => {
+    setTimeout(async () => {
+      await SplashScreen.hideAsync();
+    }, 3000);
+  }, []);
+
   return (
     <>
       <Tabs />
